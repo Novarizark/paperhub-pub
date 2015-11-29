052f6ca086221599a43b09b27a45478cf2405a5c
Novarizark <dennis-1991@qq.com>
2015-11-29 10:36:26 +0800

    login on

---
+++ login.php
@@ -0,0 +1,36 @@
+    <html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>paperhub 登录</title>
+    <link rel='stylesheet' id='buttons-css'  href='./style/buttons.min.css?ver=4.2.5' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='./style/dashicons.min.css?ver=4.2.5' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='./style/login.min.css?ver=4.2.5' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+    </head>
+    <body class="login login-action-login wp-core-ui  locale-zh-cn">
+    <div id="login">
+        <h1><a href="https://cn.wordpress.org/" title="基于WordPress" tabindex="-1">“千人计划”杨崧教授研究团队</a></h1>
+    
+<form name="loginform" id="loginform" action="http://met.sysu.edu.cn/GloCli/Team/wp-login.php" method="post">
+    <p>
+        <label for="user_login">用户名<br />
+        <input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+    </p>
+    <p>
+        <label for="user_pass">密码<br />
+        <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+    </p>
+        <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> 记住我的登录信息</label></p>
+    <p class="submit">
+        <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="登录" />
+        <input type="hidden" name="redirect_to" value="http://met.sysu.edu.cn/GloCli/Team/wp-admin/" />
+        <input type="hidden" name="testcookie" value="1" />
+    </p>
+</form>
+    
+    </div>
+
+    
+        <div class="clear"></div>
+    </body>
+    </html>
